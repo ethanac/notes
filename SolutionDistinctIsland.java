d8e01e7a7e538b67c8642147c1c01a78e386f3fc
Ethan <haoz86@gmail.com>
2021-11-14 11:31:06 -0500

    updated

--- SolutionDistinctIsland.java
+++ SolutionDistinctIsland.java
@@ -3,40 +3,53 @@ public class SolutionDistinctIsland {
      * @param grid: a list of lists of integers
      * @return: return an integer, denote the number of distinct islands
      */
-    int n = 0, m = 0;
-    Set<String> S = new HashSet<>();
-    String tmp = "";
-    boolean[][] vis = null;
-    int ix = 0, iy = 0;
-    int[] fx = new int[] { 1, -1, 0, 0 };
-    int[] fy = new int[] { 0, 0, 1, -1 };
+    int n, m = 0;
+    Set<String> islandSet = new HashSet<>();
+    int[] deltaX = {0, 1, -1, 0};
+    int[] deltaY = {1, 0, 0, -1};
+    String currentIsland = "";
+    boolean[][] visited = null;
+    int rootX, rootY = 0;
 
     public int numberofDistinctIslands(int[][] grid) {
+        if(grid == null || grid.length == 0) return 0;
+        if(grid[0] == null || grid[0].length ==0) return 0;
+
         n = grid.length;
         m = grid[0].length;
-        vis = new boolean[n][m];
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (grid[i][j] == 1 && !vis[i][j]) {
-                    tmp = "";
-                    ix = i;
-                    iy = j;
+        visited = new boolean[n][m];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1 && !visited[i][j]) {
+                    currentIsland = "";
+                    visited[i][j] = true;
+                    rootX = i;
+                    rootY = j;
                     dfs(i, j, grid);
-                    S.add(tmp);
+                    islandSet.add(currentIsland);
                 }
             }
         }
-        return S.size();
+        return islandSet.size();
     }
 
-    public void dfs(int x, int y, int grid[][]) {
-        vis[x][y] = true;
-        tmp += (x - ix) + "" + (y - iy);
-        for (int i = 0; i < 4; ++i) {
-            int nx = x + fx[i], ny = y + fy[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m || vis[nx][ny] || grid[nx][ny] == 0)
-                continue;
-            dfs(nx, ny, grid);
+    private void dfs(int x, int y, boolean[][] grid) {
+        visited[x][y] = true;
+        currentIsland += (x - rootX) + "" + (y - rootY);
+
+        for(int direction = 0; direction < 4; direction++) {
+            int newX = x + deltaX[direction];
+            int newY = y + deltaY[direction];
+
+            if(!isValid(newX, newY, visited)) continue;
+            dfs(newX, newY, grid);
         }
+    }
+
+    private boolean isValid(int x, int y, boolean[][] visited) {
+        if(x < 0 || x >=n || y < 0 || y >= m) return false;
+        if(visited[x][y] || grid[x][y] == 0) return false;
+        return true;
     }
 }
